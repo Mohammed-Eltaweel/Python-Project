ec679260d4eb3dbf3d5f9e519a0587bef0639164
Your Name <you@gmail.com>
2025-03-06 21:03:03 +0200

    first 2 projects

---
+++ project_1(simple band name generator app)/project_01.py
@@ -0,0 +1,5 @@
+print(" welcom to band name generator ")
+city_name = input("plz enter the city you grew up in \n")
+pet_name = input("enter the name of your pet\n")
+
+print("your band name could be "+city_name+" "+pet_name)
---
+++ project_2(divide the bill app)/project_02.py
@@ -0,0 +1,15 @@
+# print("Welcom to the tip calculator. ")
+# bill_amount = input("what was the total bill ?")
+# tip_percentage = input("what percentage tip would you like to give ? 10, 12,or 15 ? ")
+# number_of_people = input("how many people to splite the bill ?")
+# total_bill = float(bill_amount)+ ((int(tip_percentage)/100)*float(bill_amount))
+# result = total_bill / int(number_of_people)
+
+# print(f"Each person should pay : ${round(result,2)}")
+bill = float(input("what was the total bill ? "))
+tip = int(input("what is the tip you want to give ? 10, 12, or 15 ?"))
+num_of_people = int(input("what is the number of people to splite the bill ?"))
+totoal_bill = bill + tip/100*bill
+result = totoal_bill / num_of_people
+final_result = "{:.2f}".format(result)
+print ( f"Each person should pay : ${final_result}")
